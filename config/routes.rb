f1281ac6dc3247eef30ca83d116a47592c8c1b25
Tina Rezanezhad <tinarezanezhaad@gmail.com>
2022-12-30 01:25:35 +0100

    get game update and update board added in the home component.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,4 @@
 Rails.application.routes.draw do
   resources :games
   resources :players
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
--- front/app/components/home.js
+++ front/app/components/home.js
@@ -2,10 +2,22 @@ import Component from '@glimmer/component';
 import { action } from '@ember/object';
 import { tracked } from '@glimmer/tracking';
 import fetch from 'fetch';
-
+import { service } from '@ember/service';
+import { api } from '../services/api';
+import EmberObject from '@ember/object';
 export default class MyFormComponet extends Component {
+  @service router;
+  @service api;
   @tracked player = '';
   @tracked loadingDisplay = false;
+  @tracked isGameReady = false;
+  @tracked isFormEnable = true;
+  @tracked playerGame = undefined;
+  @tracked playerID = null;
+  @tracked playAs = '';
+  @tracked board = [];
+  @tracked result = '';
+  @tracked intervId = '';
 
   // Disable button when name in empty
   get disableSubmit() {
@@ -18,27 +30,71 @@ export default class MyFormComponet extends Component {
     const player = {
       name: this.player,
     };
-    createPlayer(player).then((response) => {
-      // error handling and showing message
-      if(response) {
-        alert('success')
+    this.api.createPlayer(player).then((response) => {
+      if (response.error) {
+        alert('wrong');
       } else {
-        alert('wrong')
+        document.cookie = 'player_id' + '=' + response.id + '; Path=/;';
+        this.isFormEnable = false;
+        this.searchGame();
       }
-      console.log(response)
     });
   }
-}
 
-const createPlayer = async function (player) {
-  const response = await fetch('http://127.0.0.1:3000/players', {
-    method: 'POST',
-    mode: 'cors',
-    cache: 'no-cache',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(player),
-  });
-  return response.json()
-};
+  @action searchGame() {
+    const intervId = setInterval(() => {
+      this.api.findGame().then((res) => {
+        if (res.id) {
+          this.board = JSON.parse(res.board);
+          this.playerGame = res;
+          this.loadingDisplay = false;
+          this.isGameReady = true;
+          document.cookie = 'game_id' + '=' + res.id + '; Path=/;';
+          this.getUpdates(this.playerGame);
+          clearInterval(intervId);
+        }
+      });
+    }, 1000);
+  }
+  @action getUpdates(game) {
+    this.intervId = setInterval(() => {
+      this.api.getGame().then((res) => {
+        this.board = JSON.parse(res.board);
+        for (let i = 0; i < 9; i++) {
+          const boardValues = this.board.join().replaceAll(',', '');
+          boardValues[i] == 'E'
+            ? (document.getElementById(i).innerText = '')
+            : (document.getElementById(i).innerText = boardValues[i]);
+        }
+      });
+    }, 1000);
+  }
+  @action onSquareClick(square) {
+    const playAs = this.playerGame.play_as;
+    // Place where player clicked
+    const clickedSquare = square.target;
+    const clickedRow = parseInt(clickedSquare.getAttribute('data-row'));
+    const clickedCol = parseInt(clickedSquare.getAttribute('data-col'));
+    // Update related item in the board
+    this.board[clickedRow][clickedCol] = playAs;
+    this.api.updateGameBoard(this.playerGame, this.board).then((res) => {
+      switch (res.result) {
+        case 1:
+          this.result = 'WINNER : X';
+          break;
+        case 2:
+          this.result = 'WINNER : O';
+          break;
+        case 0:
+          this.result = 'DRAW';
+          break;
+      }
+    });
+  }
+  @action restart() {
+    this.isFormEnable = true;
+    this.isGameReady = false;
+    this.result = null;
+    //clear cookie // start over
+  }
+}
