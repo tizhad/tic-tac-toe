2ee97b401142cb9aa21bd5c345dd59da8cb5edb1
Tina Rezanezhad <tinarezanezhaad@gmail.com>
2022-12-30 01:23:25 +0100

    Changed games table.

--- db/migrate/20221224162949_create_games.rb
+++ db/migrate/20221224162949_create_games.rb
@@ -1,11 +1,12 @@
 class CreateGames < ActiveRecord::Migration[7.0]
   def change
     create_table :games do |t|
-      t.string :player1
-      t.string :player2
+      t.integer :player1
+      t.integer :player2
+      t.string :board
       t.string :result
       t.string :status
-      t.string :turn
+      t.integer :turn
 
       t.timestamps
     end
