b1268e45d6dd4fae0c39dce13fd69ec162d74e44
Tina Rezanezhad <tinarezanezhaad@gmail.com>
2022-12-30 01:25:00 +0100

    Update and check game result added in game controller.

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,32 +1,110 @@
 class GamesController < ApplicationController
   def index
-    @games = Game.all
-    render json: @games
+    @game = Game.where(player1: params[:player_id]).
+      or(Game.where(player2: params[:player_id])).
+      and(Game.where(status: 'in-progress')).
+      order(created_at: :desc).limit(1).first()
+    if !@game
+      render json:{
+        status: 'no game'
+      }
+      return
+    end
+
+    render json: {
+      id: @game.id,
+      play_as: @game.player1 === params[:player_id].to_i ? 'X' : 'O',
+      status: @game.status,
+      board: @game.board,
+      created_at: @game.created_at,
+    }
   end
 
   def show
     @game = Game.find(params[:id])
-    render json: @game
+    if !@game
+      render json:{
+        status: 'no game'
+      }
+      return
+    end
+
+    render json: {
+      id: @game.id,
+      play_as: @game.player1 === params[:player_id].to_i ? 'X' : 'O',
+      status: @game.status,
+      board: @game.board,
+      created_at: @game.created_at,
+    }
   end
 
   def create
-    puts 'calls game create'
-    @game = Game.create(player1: params[:player1], player2: params[:player2], result:'', status: '', turn: '')
+    @game = Game.create(player1: params[:player1], player2: params[:player2], result: '', status: '', turn: '')
     render json: @game
   end
 
-  def update
-    @game = Game.find(params[:id])
 
-    if @game.update_attributes(game_param)
-      redirect_to :action => 'show', :id => @game
-    else
-      @subjects = Subject.all
-      render :action => 'edit'
+  def update
+    game = Game.find(params[:id])
+    game.board = params[:board]
+    game.save
+    result = check_game_result(game)
+    if result >= 0
+      game.status='ended'
+      game.save
     end
-    def game_param
-      params.require(:game).permit(:turn, :result, :status)
+
+    render json: {
+      result: result,
+      game: game
+    }
+  end
+
+  def check_game_result(game)
+    board = JSON.parse(game.board)
+    # Check winning conditions
+    for row in board do
+      str = row.join("")
+      if str == 'XXX'
+        return 1
+      elsif str == 'OOO'
+        return 2
+      end
+    end
+
+    for col in (0...board.length) do
+      for cel in (0...board.length) do
+        str += board[cel][col]
+      end
+      if str == 'XXX'
+        return 1
+      elsif str == 'OOO'
+        return 2
+      end
+    end
+
+    diagonal = board[0][0]+board[1][1]+board[2][2]
+    if diagonal == 'XXX'
+      return 1
+    elsif diagonal == 'OOO'
+      return 2
+    end
+
+    diagonal = board[0][2]+board[1][1]+board[2][0]
+    if diagonal == 'XXX'
+      return 1
+    elsif diagonal == 'OOO'
+      return 2
+    end
+
+    for i in (0...board.length)
+      for j in (0...board.length)
+        if board[i][j]=='E'
+          return -1
+        end
+      end
+    end
+    return 0
     end
 
   end
-end
